778c0a923888d77f63f3793892ec4953ea4634d1
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-10-31 13:45:42 +0100

    Push Update 10/31/2020, 13:45:37

--- index.bat
+++ index.bat
@@ -1,3 +1,3 @@
 @echo off
 cls
-rm -rf build&&python D:\boot\boot.py 5 compile ./ arduino:avr:uno D:\K\Coding\projects\Arduino-Lcd_Controller&&python D:\boot\boot.py 5 upload ./ COM3 arduino:avr:uno
+rm -rf build&&python D:\boot\boot.py 5 compile ./ arduino:avr:uno D\K\Coding\Arduino-Lcd_Controller&&python D:\boot\boot.py 5 upload ./ COM3 arduino:avr:uno
