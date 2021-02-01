649f2248d5b65bf34353ed522fa20b71283c8aab
Krzem <krzem5.dev@gmail.com>
2021-02-01 21:27:20 +0100

    Push Update 02/01/2021, 21:27:13

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 cls
-python D:\boot\boot.py 5 compile ./src ./build arduino:avr:uno D:\K\Coding\Arduino-Lcd_Controller\src&&python D:\boot\boot.py 5 upload ./build COM3 arduino:avr:uno
+python D:\boot\main.py 5 compile ./src ./build arduino:avr:uno D:\K\Coding\Arduino-Lcd_Controller\src&&python D:\boot\main.py 5 upload ./build COM3 arduino:avr:uno
